0f09242131939382b695da75b2152bc9b1077e60
tammuahe <tammuahe2004@gmail.com>
2025-12-22 01:06:10 +0700

    feat(backend): add endpoints

--- unigrade/src/main/java/com/tlu/unigrade/controller/AuthController.java
+++ unigrade/src/main/java/com/tlu/unigrade/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.tlu.unigrade.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
     @GetMapping("/me")
     public ResponseEntity<Void> me() {
--- unigrade/src/main/java/com/tlu/unigrade/controller/StudentController.java
+++ unigrade/src/main/java/com/tlu/unigrade/controller/StudentController.java
@@ -2,16 +2,52 @@ package com.tlu.unigrade.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tlu.unigrade.dto.course.CourseDTO;
+import com.tlu.unigrade.dto.enrollment.EnrollmentDTO;
+import com.tlu.unigrade.dto.student.StudentDTO;
+import com.tlu.unigrade.service.StudentService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@RequestMapping("/api/student")
+@RequestMapping("/student")
+@RequiredArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping("/test")
     public String test() {
         return "omfg it worked";
     }
 
+    @GetMapping
+    public ResponseEntity<StudentDTO> getStudent() {
+        return ResponseEntity.ok(studentService.getStudent());
+    }
     
+
+    @GetMapping("/enrollment")
+    public ResponseEntity<List<EnrollmentDTO>> getEnrollments(@RequestParam(required = false) String keyword) {
+        if (keyword == null || keyword.isBlank()) {
+            return ResponseEntity.ok(studentService.findAllEnrollments());
+        } else {
+            return ResponseEntity.ok(studentService.searchEnrollment(keyword));
+        }
+    }
+
+    @GetMapping("/course")
+    public ResponseEntity<List<CourseDTO>> getRequiredCourse() {
+        return ResponseEntity.ok(studentService.findAllRequiredCourses());
+    }
+
+
+
 }
