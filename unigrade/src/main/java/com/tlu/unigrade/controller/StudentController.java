d30a6f0cec7267b9a4e16ba4f743cbf772d84d6c
tammuahe <tammuahe2004@gmail.com>
2025-12-21 19:00:44 +0700

    feat(backend): add test endpoint

---
+++ unigrade/src/main/java/com/tlu/unigrade/controller/StudentController.java
@@ -0,0 +1,17 @@
+package com.tlu.unigrade.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "omfg it worked";
+    }
+
+    
+}
